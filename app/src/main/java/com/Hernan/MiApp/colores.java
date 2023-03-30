287da9645e90f31fdc1a7cb1f0e27fa64fa046d5
felipe3-A <lenovo.esucre@gmail.com>
2023-03-30 17:10:39 -0500

    Initial commit

---
+++ app/src/main/java/com/Hernan/MiApp/colores.java
@@ -0,0 +1,14 @@
+package com.Hernan.MiApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class colores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colores);
+    }
+}
